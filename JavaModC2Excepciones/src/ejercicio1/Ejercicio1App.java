e4dcd5db2268aa55bdd185649fc01ee449dc9671
Aitor Iglesias <iglesiaspubillaitor@gmail.com>
2023-03-19 18:47:41 +0100

    Ejercicio 1 Finalizado

---
+++ JavaModC2Excepciones/src/ejercicio1/Ejercicio1App.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package ejercicio1;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ * @author aitor
+ *
+ */
+public class Ejercicio1App {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner (System.in);
+		int numeroAdivinar = (int)(Math.random()*500+1);
+		boolean estado = true;
+		int contador = 0;
+		int numeroIntroducido = 0;
+		
+		do {
+			System.out.print("Introduce un numero del 1 al 500 a ver si lo adivinas: ");
+			try {
+				numeroIntroducido = sc.nextInt();
+				if(numeroIntroducido<numeroAdivinar) {
+					System.out.println("El numero que buscas es mas grande");
+				} else if (numeroIntroducido>numeroAdivinar) {
+					System.out.println("El numero que buscas es mas pequeño");
+				} else {
+					estado = false;
+					System.out.println("HAS ENCONTRADO EL NUMERO!!!, efectivamente era el "+numeroIntroducido+" y has necesitado "+contador+" intentos");
+				}
+			} catch (InputMismatchException e) {
+				System.err.println(e.getMessage().toString());
+				estado = false;
+			} finally {
+				contador++;
+			}
+		} while (estado);
+
+	}
+}
