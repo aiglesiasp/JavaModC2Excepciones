3df2936e1fdd0fe9e4d8f1d29222747b8716efc9
Aitor Iglesias <iglesiaspubillaitor@gmail.com>
2023-03-21 23:27:16 +0100

    Ejercicio 4 Completado Ejercicio 5 Pendiente lanzar excepciones

--- JavaModC2Excepciones/src/ejercicio3/Ejercicio3App.java
+++ JavaModC2Excepciones/src/ejercicio3/Ejercicio3App.java
@@ -21,13 +21,17 @@ public class Ejercicio3App {
 		System.out.println("Generando número aleatorio...");
 		System.out.println("El numero aleatorio generado es: "+numero);
 		try {
-			if(numero%2==0) {throw new MiExcepcion("El numero "+numero+" es PAR");}
-			else {throw new MiExcepcion("El numero "+numero+" es IMPAR");} 
+			comprobarParidad(numero);
 			
 		} catch(MiExcepcion ex) {
 			System.out.println(ex.getMessage());
 		}
 
+	}
+	
+	public static void comprobarParidad(int numero) throws MiExcepcion {
+		if(numero%2==0) {throw new MiExcepcion("El numero "+numero+" es PAR");}
+		else {throw new MiExcepcion("El numero "+numero+" es IMPAR");} 
 	}
 
 }
